1571537a5a18bd50885ecd7d166763df8b0b0cf0
Angel172ir16 <33010380+Angel172ir16@users.noreply.github.com>
2020-10-04 08:58:17 +0300

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -7,20 +7,20 @@ table.insert(currencies, {
     dmg = 0
 })
 
-table.insert(currencies, {
-    name = "Светопыль",
-    image = "glowstone_dust.pic",
-    id = "minecraft:glowstone_dust",
-    dmg = 0,
-    max = 100
-})
+--table.insert(currencies, {
+--    name = "Светопыль",
+--    image = "glowstone_dust.pic",
+--    id = "minecraft:glowstone_dust",
+--    dmg = 0,
+--    max = 100
+--})
 
-table.insert(currencies, {
-    name = "Железный слиток",
-    image = "iron_ingot.pic",
-    id = "minecraft:iron_ingot",
-    dmg = 0,
-    max = 200
-})
+--table.insert(currencies, {
+--    name = "Железный слиток",
+--    image = "iron_ingot.pic",
+--    id = "minecraft:iron_ingot",
+--    dmg = 0,
+--    max = 200
+--})
 
 return currencies
